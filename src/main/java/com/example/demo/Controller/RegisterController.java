aadf4ed28e8976d61cd3eb7161e1205dbb6a940c
SudoAdam <adam5530@stud.kea.dk>
2020-11-26 15:42:55 +0100

    register user, parameters pased to UserServie

--- src/main/java/com/example/demo/Controller/RegisterController.java
+++ src/main/java/com/example/demo/Controller/RegisterController.java
@@ -1,5 +1,6 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,10 +15,11 @@ public class RegisterController {
         String firstName = request.getParameter("firstName");
         String lastName = request.getParameter("lastName");
         String job = request.getParameter("job");
-        int isAdmin = 0;
         //vi skal have gjort så når man opretter sig, at den skriver direkte til databasen,
         // og så henter et bruger objekt tilbage fra databasen.
         // For så får vi nemlig userID med. med det samme.
+        UserService userService = new UserService();
+        userService.createUser(mail,firstName,lastName,job);
         return "authentication/register";
     }
 }
---
+++ src/main/java/com/example/demo/Service/UserService.java
@@ -0,0 +1,9 @@
+package com.example.demo.Service;
+
+public class UserService {
+
+    public void createUser(String mail,String firstName, String lastName, String job){
+
+
+    }
+}
