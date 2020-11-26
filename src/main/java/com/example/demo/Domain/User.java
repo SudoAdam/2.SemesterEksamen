8129929e4bd7d1d727b108c26cd15a328f69720d
RasmusBerg155 <58301697+RasmusBerg155@users.noreply.github.com>
2020-11-26 14:46:07 +0100

    User created in Domain

---
+++ src/main/java/com/example/demo/Domain/User.java
@@ -0,0 +1,94 @@
+package com.example.demo.Domain;
+
+public class User {
+
+    private int userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String projects;
+    private String isAdmin;
+    private String jobTitel;
+
+    public User(int userId, String firstName, String lastName, String email, String projects, String isAdmin, String jobTitel) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.projects = projects;
+        this.isAdmin = isAdmin;
+        this.jobTitel = jobTitel;
+    }
+
+    public User() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProjects() {
+        return projects;
+    }
+
+    public void setProjects(String projects) {
+        this.projects = projects;
+    }
+
+    public String getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(String isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    public String getJobTitel() {
+        return jobTitel;
+    }
+
+    public void setJobTitel(String jobTitel) {
+        this.jobTitel = jobTitel;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", projects='" + projects + '\'' +
+                ", isAdmin='" + isAdmin + '\'' +
+                ", jobTitel='" + jobTitel + '\'' +
+                '}';
+    }
+}
