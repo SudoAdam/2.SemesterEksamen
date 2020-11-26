18810a67476a9dfe587536b5627557443d16d095
RasmusBerg155 <58301697+RasmusBerg155@users.noreply.github.com>
2020-11-26 16:38:18 +0100

    ProjectService created

--- src/main/java/com/example/demo/Service/UserService.java
+++ src/main/java/com/example/demo/Service/UserService.java
@@ -2,7 +2,7 @@ package com.example.demo.Service;
 
 public class UserService {
 
-    public void createUser(String mail,String firstName, String lastName, String job){
+    public void createUser(String email,String firstName, String lastName, String jobTitle){
 
 
     }
