46c4717e0ff74fb83eb2bfc1f637d2132619f68a
SudoAdam <adam5530@stud.kea.dk>
2020-11-27 15:51:04 +0100

    @author added and some rearranging

--- src/main/java/com/example/demo/Controller/ProjectController.java
+++ src/main/java/com/example/demo/Controller/ProjectController.java
@@ -2,18 +2,27 @@ package com.example.demo.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class ProjectController {
 
     @GetMapping("/projectList")
-    public String showProjects(){
+    public String showProjects() {
         return "lists/projectList";
     }
 
     @GetMapping("/projectCreate")
-    public String createProject(){
+    public String createProject() {
         return "create/projectCreate";
+    }
+
+
+    @PostMapping("/projectCreate")
+    public String createingProject(WebRequest request) {
+
+        return "error";
     }
 
 }
