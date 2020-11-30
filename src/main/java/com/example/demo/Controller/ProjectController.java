5ba12a3c1de11062c1ea9b790714d2a7f6ba0209
SudoAdam <adam5530@stud.kea.dk>
2020-11-30 12:47:19 +0100

    ProjectController now request paramateres

--- src/main/java/com/example/demo/Controller/ProjectController.java
+++ src/main/java/com/example/demo/Controller/ProjectController.java
@@ -1,9 +1,13 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Service.ProjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 public class ProjectController {
@@ -26,7 +30,13 @@ public class ProjectController {
         String companyName = request.getParameter("comName");
         String contactName = request.getParameter("conName");
         String contactEmail = request.getParameter("conEmail");
-
+        String kickOffStr = request.getParameter("kickOff");
+        String deadlineStr = request.getParameter("deadline");
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate kickOff = LocalDate.parse(kickOffStr,dateTimeFormatter);
+        LocalDate deadline = LocalDate.parse(deadlineStr,dateTimeFormatter); ;
+        ProjectService projectService = new ProjectService();
+        //projectService.createProject(projectName,kickOff, deadline,);
 
         return "error";
     }
