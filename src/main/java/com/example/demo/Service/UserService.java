c5b633392a1b65afacdf77c991f97480d7b0e765
RasmusBerg155 <58301697+RasmusBerg155@users.noreply.github.com>
2020-12-01 16:35:16 +0100

    UserService created

--- src/main/java/com/example/demo/Service/UserService.java
+++ src/main/java/com/example/demo/Service/UserService.java
@@ -5,6 +5,8 @@ package com.example.demo.Service;
 
 import com.example.demo.Data.UserData;
 
+import java.time.LocalDate;
+
 public class UserService {
 
     // FIELDS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -19,5 +21,9 @@ public class UserService {
 
     public boolean createUser(String e_mail, String password, String first_name, String last_name, int job_title_id){
         return userData.createUser(e_mail, password, first_name, last_name, job_title_id);
+    }
+
+    public boolean editUser(int user_id, String e_mail, String password, String first_name, String last_name, int is_admin, int job_title_id) {
+        return userData.editUser(user_id, e_mail, password, first_name, last_name, is_admin, job_title_id);
     }
 }
