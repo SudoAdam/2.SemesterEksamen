74af3a175f247c01cfbd6f4c596d319359403da8
SudoAdam <adam5530@stud.kea.dk>
2020-12-02 12:54:56 +0100

    Task create are added to controller and a signup form made in HTML

--- src/main/java/com/example/demo/Controller/TaskController.java
+++ src/main/java/com/example/demo/Controller/TaskController.java
@@ -1,6 +1,35 @@
 package com.example.demo.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDate;
+
+@Controller
 public class TaskController {
 
-    //Gå amokka
+
+@GetMapping("/createTask")
+    public String createTask(){
+
+    return "project/createTask";
+}
+@PostMapping("/createTask")
+    public String createTask(WebRequest request){
+    String taskName = request.getParameter("taskName");
+    String taskDisp = request.getParameter("taskDisp");
+    String taskLeaderEmail = request.getParameter("taskLeaderEmail");
+    String taskKickoff = request.getParameter("kickoff");
+    String taskDeadline = request.getParameter("deadline");
+    String workingHoursSTR = request.getParameter("workingHours");
+
+    LocalDate kickoff = LocalDate.parse(taskKickoff);
+    LocalDate deadline = LocalDate.parse(taskDeadline);
+    int workingHours = Integer.parseInt(workingHoursSTR);
+
+    return "project/showTask";
+}
+
 }
