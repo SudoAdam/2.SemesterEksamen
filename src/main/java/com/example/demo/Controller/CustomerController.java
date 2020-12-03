7dc92e6e296c6a5a604c37aff100b97c560cc5a6
SudoAdam <adam5530@stud.kea.dk>
2020-12-03 13:34:41 +0100

    CustomerController updated

--- src/main/java/com/example/demo/Controller/CustomerController.java
+++ src/main/java/com/example/demo/Controller/CustomerController.java
@@ -1,11 +1,12 @@
 package com.example.demo.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 
-
+@Controller
 public class CustomerController {
     CustomerService customerService = new CustomerService();
 
@@ -19,7 +20,28 @@ public class CustomerController {
         String companyName = request.getParameter("comName");
         String contactName = request.getParameter("conName");
         String contactEmail = request.getParameter("conEmail");
+        String tel = request.getParameter("tel");
 
+
+        return "customer/listCustomer";
+    }
+
+    @GetMapping("/listCustomer")
+    public String listCustomer() {
+        return "customer/listCustomer";
+    }
+
+    @GetMapping("/editCustomer")
+    public String editCustomer() {
+        return "customer/editCustomer";
+    }
+
+    @PostMapping("/editCustomer")
+    public String editProject(WebRequest request, Model model) {
+        String companyName = request.getParameter("comName");
+        String contactName = request.getParameter("conName");
+        String contactEmail = request.getParameter("conEmail");
+        String tel = request.getParameter("tel");
 
 
         return "customer/listCustomer";
