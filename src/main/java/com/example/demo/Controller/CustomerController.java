e292c7f11a9a863d51b95947237ddf74812b3bf2
kasper <kasperkbf@hotmail.com>
2020-12-04 15:46:10 +0100

    view customer with assigned projects

--- src/main/java/com/example/demo/Controller/CustomerController.java
+++ src/main/java/com/example/demo/Controller/CustomerController.java
@@ -6,19 +6,23 @@
 package com.example.demo.Controller;
 
 import com.example.demo.Domain.Customer;
+import com.example.demo.Domain.Project;
 import com.example.demo.Service.CustomerService;
+import com.example.demo.Service.ProjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CustomerController {
     CustomerService customerService = new CustomerService();
+    ProjectService projectService = new ProjectService();
+
 
     @GetMapping("/createCustomer")
     public String createProject() {
@@ -32,7 +36,7 @@ public class CustomerController {
         String contactEmail = request.getParameter("conEmail");
         String tel = request.getParameter("tel");
 
-        customerService.createUser(companyName,contactName,contactEmail,tel);
+        customerService.createUser(companyName, contactName, contactEmail, tel);
 
         return "customer/listCustomer";
     }
@@ -40,7 +44,7 @@ public class CustomerController {
     @GetMapping("/listCustomer")
     public String listCustomer(Model model) {
         ArrayList<Customer> customerList = customerService.getCustomers();
-        model.addAttribute("customerList",customerList);
+        model.addAttribute("customerList", customerList);
         return "customer/listCustomer";
     }
 
@@ -58,6 +62,21 @@ public class CustomerController {
 
 
         return "customer/listCustomer";
+    }
+
+    // Responds to /viewCustomer?id=project_id
+    @RequestMapping(value = "/viewCustomer", method = {RequestMethod.GET, RequestMethod.POST})
+    public String viewCustomer(@RequestParam int id, Model model) throws SQLException {
+        ArrayList<Project> projectList = projectService.getProjects();
+        ArrayList<Project> projectCustomerList = new ArrayList<>();
+        for ( Project p : projectList) {
+            if (p.getCustomer_id() == id)
+            projectCustomerList.add(p);
+        }
+        model.addAttribute("customer", customerService.getCustomer(id));
+        model.addAttribute("customerProjects", projectCustomerList);
+        return "customer/viewCustomer";
+
     }
 
 }
