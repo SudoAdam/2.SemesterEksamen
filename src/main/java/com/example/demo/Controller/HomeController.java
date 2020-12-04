f76d6eea0c851d87d378265c87dfb2c0a75e979f
SudoAdam <adam5530@stud.kea.dk>
2020-12-04 15:37:53 +0100

    login in the making

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -1,14 +1,29 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.swing.*;
 
 @Controller
 public class HomeController {
+    UserService userService = new UserService();
 
     @GetMapping("/")
     public String showHome() throws Exception {
         return "authentication/login";
+    }
+
+    @PostMapping("/login")
+    public String login(WebRequest request){
+       String email =  request.getParameter("mail");
+       String password =  request.getParameter("password");
+        userService.login(email, password);
+
+        return "/viewUser";
     }
 
 }
