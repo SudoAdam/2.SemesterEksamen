c8713c10ffc416a31725213501e6bbcd32ac911c
SudoAdam <adam5530@stud.kea.dk>
2020-12-09 10:59:57 +0100

    minor fixing in subTask

--- src/main/java/com/example/demo/Data/SubTaskData.java
+++ src/main/java/com/example/demo/Data/SubTaskData.java
@@ -8,6 +8,7 @@ package com.example.demo.Data;
 import com.example.demo.Domain.SubTask;
 import com.example.demo.Domain.Task;
 import com.example.demo.Exceptions.QueryDeniedException;
+import com.example.demo.Mapper.SubTaskMapper;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -34,11 +35,11 @@ public class SubTaskData {
         preparedStatement.setInt(1, task_id);
         ResultSet resultSet = preparedStatement.executeQuery();
 
-        ArrayList<SubTask> tasks = new ArrayList<>();
+        ArrayList<SubTask> subTasks = new ArrayList<>();
         while (resultSet.next()) {
-            tasks.add((Task) subTaskMapper.create(resultSet));
+            subTasks.add((SubTask) subTaskMapper.create(resultSet));
         }
-        return tasks;
+        return subTasks;
     }
 
     public SubTask getSubTask(int sub_task_id) throws SQLException, QueryDeniedException {
