fe42742386514b57632a6323f98745fec7d06bf5
PatrickVincentHoestroem <patrick.vincent@live.dk>
2020-12-10 14:58:42 +0100

    Tests for SubTaskData class

--- src/main/java/com/example/demo/Data/SubTaskData.java
+++ src/main/java/com/example/demo/Data/SubTaskData.java
@@ -51,12 +51,27 @@ public class SubTaskData {
         }
     }
 
-    public SubTask getSubTask(int sub_task_id) throws QueryDeniedException {
+    public SubTask getSubTask(int task_id, int sub_task_id) throws QueryDeniedException {
         try {
             Connection connection = connector.getConnection();
-            String statement = "SELECT * FROM sub_tasks WHERE sub_task_id = ?";
+            String statement = "SELECT * FROM sub_tasks WHERE task_id = ? AND sub_task_id = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(statement);
-            preparedStatement.setInt(1, sub_task_id);
+            preparedStatement.setInt(1, task_id);
+            preparedStatement.setInt(2, sub_task_id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return (SubTask) subTaskMapper.create(resultSet);
+        } catch (SQLException e) {
+            throw new QueryDeniedException("Error when querying database: SQLException message: " + e.getMessage());
+        }
+    }
+
+    public SubTask getSubTask(int task_id, String sub_task_name) throws QueryDeniedException {
+        try {
+            Connection connection = connector.getConnection();
+            String statement = "SELECT * FROM sub_tasks WHERE task_id = ? AND sub_task_name = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(statement);
+            preparedStatement.setInt(1, task_id);
+            preparedStatement.setString(2, sub_task_name);
             ResultSet resultSet = preparedStatement.executeQuery();
             return (SubTask) subTaskMapper.create(resultSet);
         } catch (SQLException e) {
@@ -81,12 +96,44 @@ public class SubTaskData {
     public void editSubTask(int task_id, String sub_task_description, int sub_task_id, String sub_task_name) throws ExecuteDeniedException {
         try {
             Connection connection = connector.getConnection();
-            String statement = "UPDATE sub_tasks SET task_ID=?, sub_task_description=?, sub_task_name=? WHERE sub_task_id=?;";
+            String statement = "UPDATE sub_tasks SET task_ID=?, sub_task_description=?, sub_task_name=? WHERE task_id=? AND sub_task_id=?;";
             PreparedStatement preparedStatement = connection.prepareStatement(statement);
             preparedStatement.setInt(1, task_id);
             preparedStatement.setString(2, sub_task_description);
             preparedStatement.setString(3, sub_task_name);
-            preparedStatement.setInt(4, sub_task_id);
+            preparedStatement.setInt(4, task_id);
+            preparedStatement.setInt(5, sub_task_id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new ExecuteDeniedException("Error when requesting database: SQLException message: " + e.getMessage());
+        }
+    }
+
+    public void deleteSubTask(int task_id, int sub_task_id) throws ExecuteDeniedException {
+        try {
+            Connection connection = connector.getConnection();
+            String statement = "" +
+                    "DELETE FROM sub_tasks " +
+                    "WHERE task_id = ? AND sub_task_id = ?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(statement);
+            preparedStatement.setInt(1, task_id);
+            preparedStatement.setInt(2, sub_task_id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new ExecuteDeniedException("Error when requesting database: SQLException message: " + e.getMessage());
+        }
+    }
+
+    public void deleteSubTask(String task_name, String sub_task_name) throws ExecuteDeniedException {
+        try {
+            Connection connection = connector.getConnection();
+            String statement = "" +
+                    "DELETE s.* FROM tasks t " +
+                    "LEFT JOIN sub_tasks s ON t.task_id = s.task_id " +
+                    "WHERE t.task_name = ? AND s.sub_task_name = ?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(statement);
+            preparedStatement.setString(1, task_name);
+            preparedStatement.setString(2, sub_task_name);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             throw new ExecuteDeniedException("Error when requesting database: SQLException message: " + e.getMessage());
